7ac2fd9d00c4f5fa0b5ef396ac5e119910e33aa2
Mat <matiasrodriguez1507@gmail.com>
2024-09-06 16:42:52 -0300

    Add files via upload

---
+++ code/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<body>
+		<h1>¡Que tal Marcos!</h1>
+		<p>----------------------</p>
+		<p>|	Bienvenido      |</p>
+		<p>----------------------</p>
+		<pre id="docker-art">
+		##         .
+		## ## ##        ==
+		## ## ## ## ##    ===
+		       /"""""""""""""""""\___/ ===
+		       {                       /  ===-
+		       \______ O           __/
+		       \    \         __/
+		        \____\_______/
+
+
+		<?php $link_name = "Contador"; ?>
+		<?php $id = 2; ?>
+		<?php echo '<h2>Lo saluda Matias.</h2>'?>
+	</body>
+</html>
+
+
+
